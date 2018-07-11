92a4622fb84763f562fe364d0dbd53c361ad1532
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-11 14:28:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ if (name === "Susan")
 {
   console.log ("the name is correct")
 }
+else
+{
+  console.log ("expected the name to be Susan")
+}
 
 // Don't worry about this
 module.exports = { name, height
